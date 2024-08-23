8b992f06366b1f9d44eb943187ade7ce65a11217
Chewbacca <higgimat000@warren.k12.in.us>
2024-08-23 13:16:38 -0400

    initial commit

--- src/sumchecker.java
+++ src/sumchecker.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class sumchecker {
+public class sumchecker {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));
+        System.out.println(hasEqualSum(1, 1, 2));
+        System.out.println(hasEqualSum(1, -1, 0));
+    }
+
+    public static boolean hasEqualSum(int num1, int num2, int num3) {
+        int sum = num1 + num2;
+        if (sum == num3) {
+            return true;
+
+        } else {
+            return false;
+
+        }
+    }
 }
